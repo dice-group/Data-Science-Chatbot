ce0da4ad6528b99e9d7b05f3870ea333d210c761
rrichajalota <rrichajalota@gmail.com>
2018-04-26 19:25:15 +0200

    special characters handled, results added

--- scikit/spiders/scikit-spider.py
+++ scikit/spiders/scikit-spider.py
@@ -1,5 +1,6 @@
 import scrapy 
-import re #re.sub(r'\s+', '', text)
+import re 
+import unicodedata 
 from scikit.items import MainItem, MethodItem
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
@@ -20,8 +21,8 @@ class ScikitSpider(CrawlSpider):
         title = section.xpath("dl/dt/code/text()")
         item['libName'] = title.extract_first()[:-1]
         item['funcName'] = title.extract()[-1]
-        item['funcDesc'] = section.xpath("dl/dd/p/text()").extract_first().replace('\n', ' ')
-        item['notes'] = ' '.join([txt.strip().replace('\n', ' ') for txt in section.xpath("//p[text()='Notes']/following-sibling::p[not(@class='rubric')]//text()").extract()]) #returns text from the follwoing <p> tags as a single string, if the <p> tag is not associated with "rubric" class
+        item['funcDesc'] = unicodedata.normalize('NFKD', section.xpath("dl/dd/p/text()").extract_first().replace('\n', ' ')).encode('ascii','ignore')
+        item['notes'] = ' '.join([unicodedata.normalize('NFKD', txt.strip().replace('\n', ' ')).encode('ascii','ignore') for txt in section.xpath("//p[text()='Notes']/following-sibling::p[not(@class='rubric')]//text()").extract()]) #returns text from the follwoing <p> tags as a single string, if the <p> tag is not associated with "rubric" class
         item['methods'] = []
         
         #for sites that don't have method table and just have a paramTable, check if the mtable is empty, if it is then the first table on the site would be paramTable
@@ -45,15 +46,15 @@ class ScikitSpider(CrawlSpider):
                 if "Parameters" in fieldname.extract_first():
                     item['allFuncParams'] = row.css("td p strong::text").extract()
                     #extract the entire colBody (without including sentences starting with \n)
-                    item['funcParamBody'] = ' '.join([txt.strip().replace('\n', ' ') for txt in row.xpath("td/descendant::text()[not(starts-with(., '\n'))]").extract()])
+                    item['funcParamBody'] = ' '.join([unicodedata.normalize('NFKD', txt.strip().replace('\n', ' ')).encode('ascii','ignore') for txt in row.xpath("td/descendant::text()[not(starts-with(., '\n'))]").extract()])
 
                 if "Attributes" in fieldname.extract_first():
                     item['allFuncAttributes'] = row.css("td p strong::text").extract()
-                    item['funcAttrBody'] = ' '.join([txt.strip().replace('\n', ' ') for txt in row.xpath("td/descendant::text()[not(starts-with(., '\n'))]").extract()])
+                    item['funcAttrBody'] = ' '.join([unicodedata.normalize('NFKD', txt.strip().replace('\n', ' ')).encode('ascii','ignore') for txt in row.xpath("td/descendant::text()[not(starts-with(., '\n'))]").extract()])
 
                 if "Returns" in fieldname.extract_first():
                     item['allReturnParams'] = row.css("td p strong::text").extract()
-                    item['funcReturnBody'] = ' '.join([txt.strip().replace('\n', ' ') for txt in row.xpath("td/descendant::text()[not(starts-with(., '\n'))]").extract()])
+                    item['funcReturnBody'] = ' '.join([unicodedata.normalize('NFKD', txt.strip().replace('\n', ' ')).encode('ascii','ignore') for txt in row.xpath("td/descendant::text()[not(starts-with(., '\n'))]").extract()])
         
         #loop over methods
         if mtable:
@@ -64,9 +65,9 @@ class ScikitSpider(CrawlSpider):
                 #to extract the methodName
                 for element in method.xpath("dt/descendant::text()[not(parent::a)]").extract():
                     if "\n" not in element and '[source]' not in element:
-                        text = text + ''.join(element.replace('\n', ' '))
+                        text = text + ''.join(unicodedata.normalize('NFKD', element.replace('\n', ' ')).encode('ascii','ignore'))
                 method_item['methodName'] = text
-                method_item['methodDesc'] = ' '.join([txt.strip().replace('\n', ' ') for txt in method.xpath("dd/p//text()").extract()])
+                method_item['methodDesc'] = ' '.join([unicodedata.normalize('NFKD', txt.strip().replace('\n', ' ')).encode('ascii','ignore') for txt in method.xpath("dd/p//text()").extract()])
 
                 #extract the table inside "method" class
                 innerMethodTable = method.xpath("dd/table[@class='docutils field-list']")
@@ -77,11 +78,11 @@ class ScikitSpider(CrawlSpider):
                         
                         if "Parameters" in fieldname.extract_first():
                             method_item['methodParams'] = row.css("td p strong::text").extract()
-                            method_item['methodParamsBody'] = ' '.join([txt.strip().replace('\n', ' ') for txt in row.xpath("td//text()").extract()])
+                            method_item['methodParamsBody'] = ' '.join([unicodedata.normalize('NFKD', txt.strip().replace('\n', ' ')).encode('ascii','ignore') for txt in row.xpath("td//text()").extract()])
                         
                         if "Returns" in fieldname.extract_first():
                             method_item['methodReturns'] = row.css("td p strong::text").extract()
-                            method_item['methodReturnsBody'] = ' '.join([txt.strip().replace('\n', ' ') for txt in row.xpath("td//text()").extract()])
+                            method_item['methodReturnsBody'] = ' '.join([unicodedata.normalize('NFKD', txt.strip().replace('\n', ' ')).encode('ascii','ignore') for txt in row.xpath("td//text()").extract()])
                 
                 item['methods'].append(dict(method_item))
 
